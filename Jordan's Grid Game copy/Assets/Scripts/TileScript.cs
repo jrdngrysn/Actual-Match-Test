88220d57038d2dd747f5524d7651d9bfaabd1f78
Jordan Grayson <jtg400@nyu.edu>
2019-04-11 16:23:26 -0400

    Committing Game Code

---
+++ Jordan's Grid Game copy/Assets/Scripts/TileScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public int type;
+    public Color[] tileColors;
+
+    public Vector2Int coords;
+
+    public void SetSprite(int Rand)
+    {
+        type = Rand;
+        GetComponent<SpriteRenderer>().color = tileColors[Rand];
+    }
+
+    private void Start()
+    {
+     
+    }
+}
---
+++ Jordan's Grid Game copy/Assets/Scripts/playerScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+
+    public Vector2Int playerCoords;
+
+    public int turnsLeft = 6;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCoords = GridMaker.Instance.currentPlayerCoords;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponentInChildren<TextMesh>().text = turnsLeft.ToString();
+
+        if (turnsLeft > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (playerCoords.x - 1 >= 0)
+                {
+                    turnsLeft--;
+                    playerCoords.x--;
+                    GridMaker.Instance.newPlayerCoords = playerCoords;
+                    GridMaker.Instance.SendMessage("MovePlayer");
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (playerCoords.x + 1 <= GridMaker.WIDTH - 1)
+                {
+                    turnsLeft--;
+                    playerCoords.x++;
+                    GridMaker.Instance.newPlayerCoords = playerCoords;
+                    GridMaker.Instance.SendMessage("MovePlayer");
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (playerCoords.y - 1 >= 0)
+                {
+                    turnsLeft--;
+                    playerCoords.y--;
+                    GridMaker.Instance.newPlayerCoords = playerCoords;
+                    GridMaker.Instance.SendMessage("MovePlayer");
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (playerCoords.y + 1 <= GridMaker.HEIGHT - 1)
+                {
+                    turnsLeft--;
+                    playerCoords.y++;
+                    GridMaker.Instance.newPlayerCoords = playerCoords;
+                    GridMaker.Instance.SendMessage("MovePlayer");
+                }
+            }
+        }
+    }
+}
